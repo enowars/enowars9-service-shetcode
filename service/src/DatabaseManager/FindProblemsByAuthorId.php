e623da37f1dd6cd390b8115f5a54240e7e0b8839
nikita.filenko <nikitafil2003@gmail.com>
2025-07-15 01:20:25 +0200

    Small fix of sql vuln

--- service/src/DatabaseManager/FindProblemsByAuthorId.php
+++ service/src/DatabaseManager/FindProblemsByAuthorId.php
@@ -16,13 +16,17 @@ readonly class FindProblemsByAuthorId
 
     public function execute(?string $authorUsername): array
     {
+        try {
         $sql = "SELECT p.title as title, p.difficulty as difficulty, p.is_published as is_published, p.id as id, p.description as description FROM problems p JOIN users u ON p.author_id = u.id WHERE p.is_published = true";
         if ($authorUsername) {
             $sql .= " AND u.username = '" . $authorUsername . "'";
         }
         
         $preparedStatement = $this->entityManager->getConnection()->prepare($sql);
-        $result = $preparedStatement->executeQuery();
-        return $result->fetchAllAssociative();
+            $result = $preparedStatement->executeQuery();
+            return $result->fetchAllAssociative();
+        } catch (\Exception $e) {
+            return [];
+        }
     }
 }
